4a923c29267eddf69bbcedc94a67742f93ccd503
Mohammad Rizky Pratama <rizkymrp22@gmail.com>
2023-08-15 16:06:30 +0700

    data from .json

--- src/jsonConvert.ts
+++ src/jsonConvert.ts
@@ -4,9 +4,3 @@ import { QueryInfo } from './model';
 // Read data from data.json
 const rawData = fs.readFileSync('assets/data.json', 'utf-8');
 export const queryInfos: QueryInfo[] = JSON.parse(rawData);
-
-// console.log (queryInfos)
-
-// const data = fs.readFileSync('myfile.txt', 'utf-8');
-
-// console.log(data);
